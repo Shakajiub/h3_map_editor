f4c518364c9806ceabd454848ba114d9a28fc458
Shakajiub <oikarinenjere@gmail.com>
2023-02-02 07:39:16 +0200

    Parse and write spells and skills.

--- h3_map_editor.py
+++ h3_map_editor.py
@@ -9,7 +9,7 @@ import src.handler_01_general            as h1
 import src.handler_02_players_and_teams  as h2
 import src.handler_03_victory_conditions as h3
 import src.handler_04_heroes             as h4
-import src.handler_05_artifacts          as h5
+import src.handler_05_additional_flags   as h5
 import src.handler_06_rumors             as h6
 import src.handler_07_terrain            as h7
 
@@ -23,7 +23,7 @@ def main() -> None:
         conditions = h3.parse_victory_conditions()
         teams      = h2.parse_teams()
         hero_flags = h4.parse_hero_flags(general["map_format"])
-        artifacts  = h5.parse_artifacts()
+        ban_flags  = h5.parse_flags()
         rumors     = h6.parse_rumors()
         hero_data  = h4.parse_hero_data()
         terrain    = h7.parse_terrain(general["map_size"],
@@ -39,7 +39,7 @@ def main() -> None:
     print("\nTeams:\n\n", teams)
     print("\nVictory/Loss Conditions:\n\n", conditions)
     print("\nHeroes:\n\n", hero_flags, "\n\n", hero_data)
-    print("\nArtifacts:\n\n", artifacts)
+    print("\nBans:\n\n", ban_flags)
     print("\nRumors:\n\n", rumors)
 
     with gzip.open("output.h3m", 'wb') as io.out_file:
@@ -48,7 +48,7 @@ def main() -> None:
         h3.write_victory_conditions(conditions)
         h2.write_teams(teams)
         h4.write_hero_flags(hero_flags)
-        h5.write_artifacts(artifacts)
+        h5.write_flags(ban_flags)
         h6.write_rumors(rumors)
         h4.write_hero_data(hero_data)
         h7.write_terrain(terrain)
---
+++ src/handler_05_additional_flags.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import src.file_io as io
+
+def parse_flags() -> dict:
+    info = {
+        "artifacts": [],
+        "spells"   : [],
+        "skills"   : []
+    }
+
+    info["artifacts"]  = io.read_bits(21)
+    info["spells"]     = io.read_bits(9)
+    info["skills"]     = io.read_bits(4)
+
+    return info
+
+def write_flags(info: dict) -> None:
+    io.write_bits(info["artifacts"])
+    io.write_bits(info["spells"])
+    io.write_bits(info["skills"])
